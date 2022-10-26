2f9f2a05f52c568196303d3542795670ebbb8038
kingkinghk <65844349+kingking1223@users.noreply.github.com>
2022-10-26 13:23:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ button.addEventListener('click', () => {
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
 
         if(bmi < 18.6){
-            result.innerHTML = 'Under weight : ' + bmi;
+            result.innerHTML = 'You got ' + bmi + ', did you even eat something?';
         }else if(bmi >= 18.6 && bmi < 24.9){
             result.innerHTML = 'Normal : ' + bmi;
         }else{
-            result.innerHTML = 'Over weight : ' + bmi;
+            result.innerHTML = 'Oh man you got ' + bmi + ', go touch some grass.';
         }
     }else{
         alert('The form has errors');
